048d8bf02c1de915a4de39927595083e3a80ada6
Taimoor Hussain <taimoorhussain985@gmail.com>
2025-03-23 22:42:06 +0500

    feat: Add Loader path

--- src/common/index.jsx
+++ src/common/index.jsx
@@ -1,7 +1,7 @@
 import ErrorBoundary from "./error-boundary";
-import Loader from "./loader";
 import Button from "./button";
 import DropdownButton from "./button/dropdown-button";
 import Modal from "./modal";
 import Heading from "./heading";
-export { ErrorBoundary, Loader, Button, DropdownButton, Modal, Heading };
+import Loader from "./loader/index";
+export { ErrorBoundary, Button, DropdownButton, Modal, Heading, Loader };
