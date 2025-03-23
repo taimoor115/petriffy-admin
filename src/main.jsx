ff0604e00f56874840f47a3d2d8058e771944ad3
Taimoor Hussain <taimoorhussain985@gmail.com>
2025-03-23 20:26:54 +0500

    feat: Setup react-query

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Toaster } from "sonner";
 import "./assets/css/satoshi.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./assets/css/style.css";
+import { AuthProvider } from "./context/auth";
+import { ModalProvider } from "./context/modal";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ModalProvider>
+          <Toaster duration={2000} position="top-right" />
+          <App />
+        </ModalProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
